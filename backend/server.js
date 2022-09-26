aeb1867d0b206bd80e6b612a8369b2cbe31205d3
Shelo2001 <100028515+Shelo2001@users.noreply.github.com>
2022-09-26 13:50:38 +0400

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const productRoutes = require('./routes/productRoutes')
 const userRoutes = require('./routes/userRoutes')
 const orderRoutes = require('./routes/orderRoutes')
 const connectDB = require('./db/db')
-
+const path = require('path')
 dotenv.config()
 
 connectDB()
@@ -17,6 +17,18 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/api/v1/products', productRoutes)
 app.use('/api/v1/users', userRoutes)
 app.use('/api/v1/order', orderRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 const port = process.env.PORT || 5000
 
